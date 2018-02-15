2e2c06f8dfc75def26e384e4461ab3bce7ea34a6
Marchp <Marchp0414@Gmail.com>
2018-02-16 07:43:21 +0800

    Problem 5:部分A+B

---
+++ src/PartAPlusB.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+/**
+ * 部分A+B 题目描述 正整数A的“DA（为1位整数）部分”定义为由A中所有DA组成的新整数PA。 例如：给定A = 3862767，DA =
+ * 6，则A的“6部分”PA是66，因为A中有2个6。 现给定A、DA、B、DB，请编写程序计算PA + PB。 输入描述:
+ * 输入在一行中依次给出A、DA、B、DB，中间以空格分隔，其中0 < A, B < 1010。 输出描述: 在一行中输出PA + PB的值。 输入例子:
+ * 3862767 6 13530293 3 输出例子: 399
+ *
+ * @author Marchp
+ * @date 2018/2/16
+ */
+public class PartAPlusB {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int A = s.nextInt();
+		int a = s.nextInt();
+		int B = s.nextInt();
+		int b = s.nextInt();
+
+		int PA = getPartNumber(A, a);
+		int PB = getPartNumber(B, b);
+		System.out.println(PA + PB);
+
+	}
+
+	public static int getPartNumber(int num, int D) {
+		int P = 0;
+		int count = 0;
+		while (num > 0) {
+			if (num % 10 == D) {
+				count++;
+			}
+			num = num / 10;
+		}
+
+		if (count > 0) {
+			StringBuilder builder = new StringBuilder();
+			for (int i = 0; i < count; i++) {
+				builder.append(D);
+			}
+			P = Integer.parseInt(builder.toString());
+		}
+		return P;
+	}
+
+}
